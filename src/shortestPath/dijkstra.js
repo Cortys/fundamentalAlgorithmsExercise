11c07020950bd4cf032b7bd36757ccb9730edb0f
Clemens Damke <admin@cortys.de>
2016-12-28 23:43:07 +0100

    added heap index to have decreaseKey costs of O(log n) instead of O(n)
    this required implementing a custom percolateUp method, since fastpriorityqueue is not flexible enough.

--- src/shortestPath/dijkstra.js
+++ src/shortestPath/dijkstra.js
@@ -2,15 +2,38 @@
 
 const PriorityQueue = require("fastpriorityqueue");
 
+// Custom percolateUp implementation, since fastpriorityqueue does not offer one
+// that returns the new position of the percolated element.
+function percolateUp(queue, i) {
+	const myval = queue.array[i];
+
+	while(i > 0) {
+		const p = i - 1 >> 1; // eslint-disable-line no-bitwise
+		const ap = queue.array[p];
+
+		if(!queue.compare(myval, ap))
+			break;
+
+		queue.array[i] = ap;
+		i = p;
+	}
+
+	queue.array[i] = myval;
+
+	return i;
+}
+
 function dijkstra(graph, start) {
 	const distance = new Map();
 	const marked = new WeakSet();
+	const index = new WeakMap();
 	const queue = new PriorityQueue((a, b) => distance.get(a) < distance.get(b));
 
 	graph.nodes.forEach(node => distance.set(node, Infinity));
 	distance.set(start, 0);
 
 	queue.heapify([...graph.nodes]);
+	queue.array.forEach((node, index) => marked.set(node, index));
 
 	while(!queue.isEmpty()) {
 		const node = queue.poll();
@@ -27,7 +50,7 @@ function dijkstra(graph, start) {
 				return;
 
 			distance.set(neighbor, newDist);
-			queue._percolateUp(queue.array.indexOf(neighbor)); // eslint-disable-line no-underscore-dangle
+			index.set(neighbor, percolateUp(queue, index.get(neighbor)));
 		});
 	}
 
