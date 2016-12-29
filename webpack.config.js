870c734b7cca8c4ff9b1b655ad2cfb3b85feb396
Clemens Damke <admin@cortys.de>
2016-12-29 23:47:35 +0100

    minified build + outputs ES5 syntax with ES6 builtins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 "use strict";
 
+const webpack = require("webpack");
+
 module.exports = {
 	target: "node",
 	entry: "./src/index.js",
 	output: {
 		path: "./build",
-		filename: "bundle.js"
+		filename: "johnson.js"
 	},
 	module: {
 		loaders: [{
 			test: /\.js$/,
 			loader: "babel-loader"
 		}]
-	}
+	},
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			include: /johnson/,
+			minimize: true
+		}),
+		new webpack.BannerPlugin("#!/usr/bin/env node", {
+			raw: true
+		})
+	]
 };
